53c33723fcee79ebf6fc29df229ff99cfc02731b
Chris Norman <chris.norman2@googlemail.com>
2023-12-17 18:14:50 +0000

    Play a sound at the position of the nearest wall.

--- lib/src/widgets/game_walls_builder.dart
+++ lib/src/widgets/game_walls_builder.dart
@@ -14,6 +14,8 @@ class GameWallsBuilder extends StatefulWidget {
   const GameWallsBuilder({
     required this.walls,
     required this.builder,
+    this.wallCloseSound,
+    this.wallCloseDistance = 10.0,
     super.key,
   });
 
@@ -22,6 +24,12 @@ class GameWallsBuilder extends StatefulWidget {
 
   /// The builder for this widget.
   final BuildContextValueBuilder<GameWallsContext> builder;
+
+  /// The sound to play when walls are close.
+  final Sound? wallCloseSound;
+
+  /// The distance at which the [wallCloseSound] should play.
+  final double wallCloseDistance;
 
   /// Create state for this widget.
   @override
@@ -33,8 +41,11 @@ class GameWallsBuilderState extends State<GameWallsBuilder> {
   /// The walls context to send to the builder.
   late final GameWallsContext gameWallsContext;
 
-  /// The source to play wall sounds through.
-  late final Source3D wallSoundsSource;
+  /// The source to play close wall sounds through.
+  late final Source3D wallCloseSoundSource;
+
+  /// The generator which plays wall sounds.
+  Generator? wallCloseSoundGenerator;
 
   /// Initialise state.
   @override
@@ -49,7 +60,15 @@ class GameWallsBuilderState extends State<GameWallsBuilder> {
       }
     }
     gameWallsContext = GameWallsContext(walls: walls, onMove: onMove);
-    wallSoundsSource = context.synthizerContext.createSource3D();
+    wallCloseSoundSource = context.synthizerContext.createSource3D();
+  }
+
+  /// Dispose of the widget.
+  @override
+  void dispose() {
+    super.dispose();
+    wallCloseSoundSource.destroy();
+    wallCloseSoundGenerator?.destroy();
   }
 
   /// Build a widget.
@@ -58,5 +77,27 @@ class GameWallsBuilderState extends State<GameWallsBuilder> {
       widget.builder(context, gameWallsContext);
 
   /// The function to call when the player has moved.
-  void onMove(final Point<double> coordinates) {}
+  Future<void> onMove(final Point<double> coordinates) async {
+    final nearestWall = gameWallsContext.getNearestWall(coordinates.floor());
+    if (nearestWall != null &&
+        nearestWall.distance <= widget.wallCloseDistance) {
+      final wallCoordinates = nearestWall.coordinates;
+      wallCloseSoundSource.position.value = Double3(
+        wallCoordinates.x.toDouble(),
+        wallCoordinates.y.toDouble(),
+        0.0,
+      );
+      final sound = widget.wallCloseSound;
+      if (sound != null) {
+        wallCloseSoundGenerator = await context.playSound(
+          sound: sound,
+          source: wallCloseSoundSource,
+          destroy: false,
+        );
+      }
+    } else {
+      wallCloseSoundGenerator?.destroy();
+      wallCloseSoundGenerator = null;
+    }
+  }
 }

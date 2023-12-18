9ad63c107afe5de29fd95c7b02bcf58795ad1c9c
Chris Norman <chris.norman2@googlemail.com>
2023-12-18 12:48:53 +0000

    Add coordinates for all wall corners.

--- lib/src/walls/game_wall.dart
+++ lib/src/walls/game_wall.dart
@@ -17,6 +17,24 @@ class GameWall {
   /// The final coordinates of this wall.
   final Point<int> endCoordinates;
 
+  /// The coordinates at the southwest corner.
+  ///
+  /// These coordinates are identical to the [startCoordinates], but are here
+  /// for completeness.
+  Point<int> get southwestCorner => startCoordinates;
+
+  /// The coordinates of the northeast corner
+  ///
+  /// These coordinates are identical to the [endCoordinates], but are here for
+  /// completeness.
+  Point<int> get northeastCorner => endCoordinates;
+
+  /// The coordinates of the northwest corner
+  Point<int> get northwestCorner => Point(startCoordinates.x, endCoordinates.y);
+
+  /// The coordinates of the southeast corner
+  Point<int> get southeastCorner => Point(endCoordinates.x, startCoordinates.y);
+
   /// The collision sound to use.
   ///
   /// If [collisionSound] is `null`, no sound will play when this wall is

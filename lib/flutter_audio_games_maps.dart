4012c3a379da88e44b9d04015ffb630f0345b8fc
Chris Norman <chris.norman2@googlemail.com>
2024-01-10 18:16:56 +0000

    Export `getDefaultGameMapScreenShortcuts`.

--- lib/flutter_audio_games_maps.dart
+++ lib/flutter_audio_games_maps.dart
@@ -6,6 +6,7 @@ export 'src/collisions/on_collide_mode.dart';
 export 'src/game_objects/game_object.dart';
 export 'src/game_objects/game_object_context.dart';
 export 'src/game_objects/game_object_source.dart';
+export 'src/screens/default_game_map_screen_shortcuts.dart';
 export 'src/screens/game_map_screen.dart';
 export 'src/walls/game_wall.dart';
 export 'src/walls/game_walls_context.dart';
